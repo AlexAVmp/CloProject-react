b6adfc78c9ba23d7e75eea289e4e01d6e5a328f8
AlexAVmp <al.averyn.new@gmail.com>
2021-07-14 23:44:47 +0300

    feat: first files have been added

---
+++ cloproject-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Header from './components/pages/Header';
+import './normalize.css';
+
+function App() {
+  return (
+      <Header />
+  );
+}
+
+export default App;
---
+++ cloproject-react/src/components/pages/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '/work/CloProject-react/cloproject-react/src/components/Header.css';
+
+function Header() {
+    return (
+        <section className="firstsection">
+            <div className="firstsection__items">
+                <h1>There is no other platforms for you as like </h1>
+                <button>TRY NOW</button>
+            </div>
+        </section>
+    );
+}
+
+export default Header;
